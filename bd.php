ef1599af549aa0f4a736f3ff61a09ee5ebb5b7b1
ewbriao1978 <eduardo.briao@gmail.com>
2025-03-26 15:04:42 -0300

    Add files

--- bd.php
+++ bd.php
@@ -1,22 +1,34 @@
 <?php
 $conexao = mysqli_connect("localhost","root","","aulabackend") or print (mysqli_error());
 
-$query = "SELECT nome,cidade,idade FROM clientes where idade >60 ORDER BY nome ASC ";
+$query = "SELECT nome,cidade,idade FROM clientes ORDER BY nome ASC ";
 
 $resultado = mysqli_query($conexao,$query);
 ?>
 
+
+<a href="/inserir_dados.php"> Inserir novo cliente.</a>
 <table border="1"><tr>
 	<td><b>Nome</b></td>
 	<td><b>Cidade</b></td>
     <td><b>Idade</b></td>
+    <td><b> # </b><td>
 </tr>
 
 <?php
 
 
 while($linha = mysqli_fetch_array($resultado)){
-    echo "<tr><td>".$linha['nome']."</td><td>".$linha['cidade']."</td><td>".$linha['idade']."</td></tr><br />";
+    echo "<tr><td>".$linha['nome']."</td><td>".$linha['cidade']."</td><td>".$linha['idade']."</td>
+    <td>
+        <form action='bd.php' method='POST'>
+            <button type='submit'> Remover </button>
+        </form>
+    </td>
+
+    </tr>
+    
+    <br />";
 }
 
 mysqli_close($conexao);
