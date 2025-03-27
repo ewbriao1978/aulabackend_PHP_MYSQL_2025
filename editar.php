554621985278b65bf673782fc8dc829c234d5e96
ewbriao1978 <eduardo.briao@gmail.com>
2025-03-27 14:19:33 -0300

    editar form

--- editar.php
+++ editar.php
@@ -1,36 +1,34 @@
-<h1> Edição de dados </h1>
-<form action="editar.php" method="POST">
-
-        <label>Nome: </label><br>
-        <input type="text" name="nome" required><br> 
-        <label> Cidade:  </label><br>
-        <input type="text"  name="cidade" required><br>
-        <label> Idade: </label><br>
-        <input type="number" name="idade" required><br> 
-
-        <button type="submit"> Enviar </button>
-    </form>
 
 <?php
-// Trocar esse if ?
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    $nome=$_POST["nome"];
-    $cidade=$_POST["cidade"];
-    $idade=$_POST['idade'];
+    $idBuscaDados = $_POST['id_para_editar'];
     $conexao = mysqli_connect("localhost","root","","aulabackend") or print (mysqli_error());
 
-    $query = "insert into clientes(nome, cidade, idade) values ('". $nome."','".$cidade."',".$idade.")";
+    $query = "SELECT id, nome,cidade,idade FROM clientes WHERE id=".$idBuscaDados;
 
-    echo $query;
+    $resultado = mysqli_query($conexao,$query);
+   // echo "resultado:";
 
+    $linha = mysqli_fetch_array($resultado);
 
-//    $resultado = mysqli_query($conexao,$query);
-    echo "<h3> Dados inseridos com sucesso!</h3><br>";
-    
+  //  echo "nome: ".$linha['nome'];
 
-    mysqli_close($conexao);
-}
-
+  //echo "o id que peguei é".$idBuscaDados;
 ?>
+
+<h1> Edição de dados </h1>
+<form action="atualizar.php" method="POST">
+
+        <label>Nome: </label><br>
+        <input type="text" name="nome" value="<?=$linha['nome']?>"  required><br> 
+        <label> Cidade:  </label><br>
+        <input type="text"  name="cidade" value="<?=$linha['cidade']?>" required><br>
+        <label> Idade: </label><br>
+        <input type="number" name="idade" value=<?=$linha['idade']?> required><br> 
+
+        <button type="submit"> Atualizar! </button>
+    </form>
+
+
+
+
 <a href="/bd.php"> Voltar para a tela principal </a>
